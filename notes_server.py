677d19ae619c44b0189d495a6ff1702a95bdde93
Tiago Pais <tiago.pais@gmail.com>
2012-03-13 00:53:37 +0000

    Initial version for Notes Server

---
+++ notes_server.py
@@ -0,0 +1,51 @@
+import json
+from pymongo import Connection, json_util
+from bottle import get, post, run, request, response
+from pymongo.objectid import ObjectId
+
+def get_notes_collection():
+    notesaround_db = Connection().notesaround
+    notes = notesaround_db.notes
+    return notes
+
+
+@get('/api/notes')
+def get_notes():
+
+    response.content_type = 'application/json'
+
+    notes = get_notes_collection()
+
+    most_recent_notes = list(notes.find())
+
+    return json.dumps(most_recent_notes, default=json_util.default)
+
+
+@get('/api/note/<note>')
+def get_note(note):
+
+    response.content_type = 'application/json'
+
+    notes = get_notes_collection()
+    single_note = notes.find({"_id": ObjectId(note)})
+
+    if not single_note:
+      return json.dumps({})
+
+    return  json.dumps(dict(single_note), default=json_util.default)
+
+@post('/api/note')
+def new_note():
+
+    response.content_type = 'application/json'
+
+    newnote = request.forms.get('note')
+    newnote_json = json.loads(newnote)
+
+    notes = get_notes_collection()
+
+    notes.insert(newnote_json)
+
+    return json.dumps(newnote_json, default=json_util.default)
+
+run(host='localhost', port=8080)
---
+++ test/api.py
@@ -0,0 +1,44 @@
+import json
+from restclient import GET, POST
+import unittest
+
+__author__ = 'Tiago Pais'
+
+class TestPOST(unittest.TestCase):
+
+    def test_get_allnotes(self):
+        responseheaders, response_allnotes = GET("http://localhost:8080/api/notes",
+                                async = False, resp=True)
+
+        self.assertTrue(response_allnotes)
+        self.assertEquals(200, responseheaders.status)
+        self.assertEquals("application/json", responseheaders['content-type'])
+
+        django_notes = json.loads(response_allnotes)
+
+        self.assertTrue(django_notes)
+
+    def test_get_onenote(self):
+        responseheaders, response_onenote = GET("http://localhost:8080/api/note/4f5e81e06c83ee214c000000",
+                               async = False, resp=True)
+
+        self.assertTrue(response_onenote)
+        self.assertEquals(200, responseheaders.status)
+        self.assertEquals("application/json", responseheaders['content-type'])
+
+        django_note = json.loads(response_onenote)
+
+        self.assertTrue(django_note)
+
+    def test_newnote(self):
+        responseheaders, response = POST("http://localhost:8080/api/note",
+                        params = {'note' : '{"text" : "Note test"}'},
+                        async = False, resp=True)
+
+        self.assertTrue(response)
+        self.assertEquals(200, responseheaders.status)
+        self.assertEquals("application/json", responseheaders['content-type'])
+
+        django_note = json.loads(response)
+
+        self.assertTrue(django_note)
