0a256c989f3a4fcce04a17621f558442465af4d1
Tiago Pais <tiago.pais@gmail.com>
2012-03-18 18:53:29 +0000

    Cleaning code for workshop.

--- js/notesaround-effects.js
+++ js/notesaround-effects.js
@@ -3,19 +3,17 @@ NotesAround_FX = {
     last_x : 0,
     last_y : 0,
     last_z : 0,
+
     last_x2 : 0,
     last_y2 : 0,
     last_z2 : 0,
 
-
     bindToAccelerometer : function(callback) {
         if (window.DeviceMotionEvent) {
             window.addEventListener('devicemotion', function (event) {
-                callback(event.acceleration.x * 2,
-                    event.acceleration.y * 2,
-                    event.acceleration.z * 2);
+                callback(event.acceleration.x * 2, event.acceleration.y * 2, event.acceleration.z * 2);
             }, true);
-        } else  if (window.DeviceOrientationEvent) {
+        } else if (window.DeviceOrientationEvent) {
                 window.addEventListener("deviceorientation", function (event) {
                     callback(event.beta, event.gamma,event.alpha);
                 }, true);
@@ -28,7 +26,7 @@ NotesAround_FX = {
 
     eraseOnTilt : function(accx,accy,accz) {
         var sensitivity = 31;
-        //console.log('Posicao ' + accx + ';' +accy+ ';' + accz);
+
         this.last_x = accx;
         this.last_y = accy;
         this.last_z = accz;
@@ -42,7 +40,6 @@ NotesAround_FX = {
                 this.last_z-this.last_z2);
 
             if (change > sensitivity) {
-                console.log('bouncing');
                 app.bounceMarkers();
             }
 
@@ -51,19 +48,6 @@ NotesAround_FX = {
             this.last_y2 = this.last_y;
             this.last_z2 = this.last_z;
         }, 150);
-    },
-    logalaisso: function(event) {
-        if(event.type==='deviceorientation') {
-            console.log('teve evento ' + event.beta + ';' +event.gamma + ';' + event.alpha);
-        } else if(event.type==='devicemotion') {
-            console.log('teve evento ' + event.accelerationIncludingGravity.x  + ';' +event.accelerationIncludingGravity.y + ';' + event.accelerationIncludingGravity.z);
-        } else if(event.type==='MozOrientation') {
-            console.log('teve evento ' + event.orientation.x + ';' +event.orientation.y + ';' + event.orientation.z);
-        } else {
-            console.log('outro ' + event.type);
-        }
     }
-
-
 }
 
