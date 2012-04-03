3fc18ed49168b6489d4db6c01e9bff743ad8618c
bteixeira <bteixeira666@gmail.com>
2012-04-03 22:03:01 +0100

    Refactored checkpoints 1, 2 and 3

--- checkpoints/checkpoint2/notesaround.js
+++ checkpoints/checkpoint2/notesaround.js
@@ -1,49 +1,42 @@
-var app =  new function () {
-    var myOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl: false,
-        zoomControl: true,
-        panControl : false,
-        rotateControl : false,
-        scaleControl : false,
-        overviewMapControl : false
-    };
-    var me = this;
-    var appMap;
+var app = {
+	init: function () {
+		var myOptions = {
+			center: new google.maps.LatLng(-34.397, 150.644),
+			zoom: 5,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControl: false,
+			zoomControl: true,
+			panControl: false,
+			rotateControl: false,
+			scaleControl: false,
+			overviewMapControl: false
+		};
+		this.appMap = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		this.updateCurrentPosition();
+	},
 
-    return {
-        init : function() {
-            var that = this;
-            me.appMap = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-            this.updateCurrentPosition();
-        },
-
-        updateCurrentPosition : function() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function( position ){
-                        // Log that this is the initial position.
-                        console.log( "Position Found" );
-                        var calculatedPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                        if (me.currentPosition === undefined) {
-                            me.appMap.setCenter(calculatedPosition);
-                            me.appMap.setZoom(14);
-                        }
-
-                        me.currentPosition = calculatedPosition;
-                    },
-                    function( error ){
-                        console.log( "Something went wrong: ", error );
-                        me.currentPosition =   new google.maps.LatLng(-34.397, 150.644);
-                    },
-                    {
-                        maximumAge:Infinity,
-                        enableHighAccuracy: true
-                    } );
-            } else {
-                alert('I guess this browser does not support geolocation!')
-            }
-        }
-    }
-}();
+	updateCurrentPosition: function () {
+		var me = this;
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+						console.log("Position Found");
+						var calculatedPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+						if (!me.currentPosition) {
+							me.appMap.setCenter(calculatedPosition);
+							me.appMap.setZoom(14);
+						}
+						me.currentPosition = calculatedPosition;
+					},
+					function (error) {
+						console.log("Something went wrong: ", error);
+						me.currentPosition = new google.maps.LatLng(-34.397, 150.644);
+					},
+					{
+						maximumAge: Infinity,
+						enableHighAccuracy: true
+					});
+		} else {
+			alert('I guess this browser does not support geolocation!')
+		}
+	}
+};
--- checkpoints/checkpoint3/notesaround.js
+++ checkpoints/checkpoint3/notesaround.js
@@ -1,76 +1,71 @@
-var app =  new function () {
-    var myOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl: false,
-        zoomControl: true,
-        panControl : false,
-        rotateControl : false,
-        scaleControl : false,
-        overviewMapControl : false
-    };
-    var me = this;
-    var appMap;
+var app = {
 
-    return {
-        init : function() {
-            var that = this;
-            me.appMap = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-            this.updateCurrentPosition();
-            this.fetchNotes();
-        },
+//	var me = this;
+//	var appMap;
 
-        updateCurrentPosition : function() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function( position ){
-                        // Log that this is the initial position.
-                        console.log( "Position Found" );
-                        var calculatedPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                        if (me.currentPosition === undefined) {
-                            me.appMap.setCenter(calculatedPosition);
-                            me.appMap.setZoom(14);
-                        }
+	init: function () {
+		var myOptions = {
+			center: new google.maps.LatLng(-34.397, 150.644),
+			zoom: 5,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControl: false,
+			zoomControl: true,
+			panControl: false,
+			rotateControl: false,
+			scaleControl: false,
+			overviewMapControl: false
+		};
+		this.appMap = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		this.updateCurrentPosition();
+		this.fetchNotes();
+	},
 
-                        me.currentPosition = calculatedPosition;
-                    },
-                    function( error ){
-                        console.log( "Something went wrong: ", error );
-                        me.currentPosition =   new google.maps.LatLng(-34.397, 150.644);
-                    },
-                    {
-                        maximumAge:Infinity,
-                        enableHighAccuracy: true
-                    } );
-            } else {
-                alert('I guess this browser does not support geolocation!')
-            }
-        },
+	updateCurrentPosition: function () {
+		var me = this;
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+						console.log("Position Found");
+						var calculatedPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+						if (me.currentPosition === undefined) {
+							me.appMap.setCenter(calculatedPosition);
+							me.appMap.setZoom(14);
+						}
+						me.currentPosition = calculatedPosition;
+					},
+					function (error) {
+						console.log("Something went wrong: ", error);
+						me.currentPosition = new google.maps.LatLng(-34.397, 150.644);
+					},
+					{
+						maximumAge: Infinity,
+						enableHighAccuracy: true
+					});
+		} else {
+			alert('I guess this browser does not support geolocation!')
+		}
+	},
 
-        fetchNotes : function() {
-            $.ajax({
-                url: 'http://www.notesaround.com/api/notes',
-                context: document.body
-            }).done(function (notes, success, xhr, handle) {
-                    for (note in notes) {
-                        if (notes[note].note) {
-                            that.displayNote(notes[note]);
-                        }
-                    }
-                });
-        },
+	fetchNotes: function () {
+		var me = this;
+		$.ajax({
+			url: 'http://www.notesaround.com/api/notes',
+			context: document.body
+		}).done(function (notes, success, xhr, handle) {
+					for (note in notes) {
+						if (notes[note].note) {
+							me.displayNote(notes[note]);
+						}
+					}
+				});
+	},
 
-        displayNote: function(note) {
-            var that = this;
-            var noteMarker = new google.maps.Marker({
-                position: new google.maps.LatLng(note.loc[0],
-                    note.loc[1]),
-                map: me.appMap,
-                animation: google.maps.Animation.DROP,
-                icon: 'marker.png',
-                title : note.note
-            });
-            me.markers.push(noteMarker);
-        }
-    }
-}();
+	displayNote: function (note) {
+		var noteMarker = new google.maps.Marker({
+			position: new google.maps.LatLng(note.loc[0], note.loc[1]),
+			map: this.appMap,
+			animation: google.maps.Animation.DROP,
+			icon: 'marker.png',
+			title: note.note
+		});
+	}
+};
